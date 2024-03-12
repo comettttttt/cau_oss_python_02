3ca95652b9172527b43e3bc13bbb3504b565f6c3
comettttttt <163105167+comettttttt@users.noreply.github.com>
2024-03-12 16:22:41 +0900

    Create volume_calc.py
    2주차 과제 완료

---
+++ volume_calc.py
@@ -0,0 +1,5 @@
+ultra_legend_oh_my_god_width =float(input("멋진 가로:"))
+ultra_legend_oh_yeah_height = float(input("예쁜 세로:"))
+very_ultra_highhh = float(input("63빌딩의 높이는 ?:"))
+totalllll_scoreeeee=ultra_legend_oh_my_god_width * ultra_legend_oh_yeah_height * very_ultra_highhh
+print("박스의 부피는", totalllll_scoreeeee, "입니다.")
