097f0266e2faf0489618bcf279ed6edccfa76489
comettttttt <hyesung5515@naver.com>
2024-03-26 17:37:37 +0900

    git 설치완료

--- volume_calc.py
+++ volume_calc.py
@@ -3,3 +3,9 @@ ultra_legend_oh_yeah_height = float(input("예쁜 세로:"))
 very_ultra_highhh = float(input("63빌딩의 높이는 ?:"))
 totalllll_scoreeeee=ultra_legend_oh_my_god_width * ultra_legend_oh_yeah_height * very_ultra_highhh
 print("박스의 부피는", totalllll_scoreeeee, "입니다.")
+print("박스의 부피는", totalllll_scoreeeee, "입니다.")
+
+
+
+
+print("박스의 부피는", totalllll_scoreeeee, "입니다.")
